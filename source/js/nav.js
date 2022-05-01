233f1a9c693654361638734572abeb7e50c8fdae
Shchipanov <chips_80@mail.ru>
2022-05-01 19:01:50 +0400

    делает справйт и картинку webp

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,33 +1,8 @@
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-header__toggle");
-// var link = document.querySelectorAll(".main-nav__list a");
-// var navToggleclosed =  document.querySelector(".main-header__toggle--closed");
-
-// navToggle.addEventListener("click", function (evt) {
-// evt.preventDefault();
-// navMain.classList.add("main-nav--opened");
-// });
-
-// link.addEventListener("click", function (evt) {
-// evt.preventDefault();
-// navToggle.classList.add(".main-header__toggle--closed");
-// });
-
-// closed.addEventListener("click", function (evt) {
-// evt.preventDefault();
-// navMain.classList.add("main-nav--closed");
-// });
 
 navMain.classList.remove("main-nav--nojs");
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    console.log("Открывает меню");
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    console.log("Закрывает меню");
-  }
+  navMain.classList.toggle('main-nav--opened');
 });

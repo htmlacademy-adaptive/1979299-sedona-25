c7dc1786e6e66d3737775b270b43ee0f1bdfcefc
Shchipanov <chips_80@mail.ru>
2022-05-05 10:09:02 +0400

    сохранение

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,5 +1,5 @@
-var navMain = document.querySelector(".main-nav");
-var navToggle = document.querySelector(".main-header__toggle");
+let navMain = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-header__toggle");
 
 navMain.classList.remove("main-nav--nojs");
 

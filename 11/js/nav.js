06f25e722ac9e7d3332092be7d14ee1e655ffafc
Keks <keks@htmlacademy.ru>
2022-05-11 12:12:44 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/nav.js
+++ 11/js/nav.js
@@ -1,8 +1,1 @@
-let navMain = document.querySelector(".main-nav");
-let navToggle = document.querySelector(".main-header__toggle");
-
-navMain.classList.remove("main-nav--nojs");
-
-navToggle.addEventListener('click', function () {
-  navMain.classList.toggle('main-nav--opened');
-});
+let navMain=document.querySelector(".main-nav"),navToggle=document.querySelector(".main-header__toggle");navMain.classList.remove("main-nav--nojs"),navToggle.addEventListener("click",(function(){navMain.classList.toggle("main-nav--opened")}));
